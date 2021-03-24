7bfe4e59674bcf0f2199844297d8ebc327858edb
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2021-03-23 22:36:17 -0700

    Sidebar almost complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,26 +3,32 @@ import './Sidebar.css'
 import TwitterIcon from '@material-ui/icons/Twitter'
 import HomeIcon from '@material-ui/icons/Home'
 import SidebarOption from './SidebarOption'
+import SearchIcon from '@material-ui/icons/Search'
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone'
+import MailOutlineIcon from '@material-ui/icons/MailOutline'
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+import ListAltIcon from '@material-ui/icons/ListAlt'
+import PermIdentityIcon from '@material-ui/icons/PermIdentity'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import { Button } from '@material-ui/core'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      {/* Twitter icon */}
       <TwitterIcon />
 
-      <SidebarOption Icon={HomeIcon} text='Home' />
-      <SidebarOption text='Explore' />
-      <SidebarOption text='Notifications' />
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
+      <SidebarOption active Icon={HomeIcon} text='Home' />
+      <SidebarOption Icon={SearchIcon} text='Explore' />
+      <SidebarOption Icon={NotificationsNoneIcon} text='Notifications' />
+      <SidebarOption Icon={MailOutlineIcon} text='Messages' />
+      <SidebarOption Icon={BookmarkBorderIcon} text='Bookmarks' />
+      <SidebarOption Icon={ListAltIcon} text='List' />
+      <SidebarOption Icon={PermIdentityIcon} text='Profile' />
+      <SidebarOption Icon={MoreHorizIcon} text='More' />
 
-      {/* Button -> Tweet */}
+      <Button variant='outlined' className='sidebar__tweet' fullWidth>
+        Tweet
+      </Button>
     </div>
   )
 }
