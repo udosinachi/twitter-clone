605cb5fac4cb2eb479777c40caff8bec6f2fa19a
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2021-03-29 11:09:48 +0100

    mobile views

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -10,6 +10,7 @@ import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
 import ListAltIcon from '@material-ui/icons/ListAlt'
 import PermIdentityIcon from '@material-ui/icons/PermIdentity'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import CreateIcon from '@material-ui/icons/Create'
 import { Button, Avatar } from '@material-ui/core'
 
 const Sidebar = () => {
@@ -28,6 +29,9 @@ const Sidebar = () => {
 
       <Button variant='outlined' className='sidebar__tweet' fullWidth>
         Tweet
+      </Button>
+      <Button variant='outlined' className='sidebar__pen'>
+        <CreateIcon />
       </Button>
 
       <div className='profile'>
